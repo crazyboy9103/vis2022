04bc4f8562ce93d00133427915ca1cc82a31f6f7
edw2n <esbaek@hcil.snu.ac.kr>
2022-12-03 14:09:02 +0900

    plotting modules and an example

---
+++ plot_utils/bubble_maker.py
@@ -0,0 +1,103 @@
+from plotly.offline import plot
+import plotly.graph_objects as go
+import seaborn as sns
+from pprint import pprint as pp
+import circlify as circ
+from collections import defaultdict
+import math
+
+class BubbleMaker:
+  palette = sns.color_palette("GnBu_d",10).as_hex()
+  default_bubble_kwargs = {'type': 'circle', 'xref': 'x', 'yref': 'y' }
+  padding = 0.01
+  
+  def __init__(self, palette=None):
+    if palette:
+      self.palette = palette
+    self.fig = None
+  
+  def cluster_to_bubble(self, cluster):
+    bubble_data = {
+        'id': cluster['cid'],
+        'datum': cluster['number'],
+        'color': self.palette[cluster['score']],
+    }
+    return bubble_data
+
+  def gen_bubble(self, cluster_data):
+    '''
+    :gnerate bubble data from cluster_data
+
+    input:
+    - cluster_data : list of clusters 
+    - cluster: {'score':int, 'cid':string, 'number':int, 'c_members': list of nid(int)}
+
+    output:
+    - bubbles : list of bubble data for plotting (position info, radius, color, ...)
+    '''
+
+    #grouping as score
+    children_info = defaultdict(list)
+    for cluster in cluster_data:
+      bubble = self.cluster_to_bubble(cluster)
+      children_info[cluster['score']].append(bubble)
+
+    #gen data as bubble format from cluster_data
+    bubble_data = []
+    for k, children in children_info.items():
+      sc_bubble_data = {}
+      sc_bubble_data['id'] = f'sc-{k}'
+      sc_bubble_data['children'] = children
+      sc_bubble_data['datum'] = sum([child['datum'] for child in children])
+      bubble_data.append(sc_bubble_data)
+      bubbles = circ.circlify(bubble_data, show_enclosure=False)
+    return bubbles
+  
+  #draw packed bubble chart as plotly fig
+  def plot_bubbles(self, bubbles, padding=None, fig=None):
+    
+    if fig:
+      self.fig = fig
+    elif not self.fig:
+      self.fig = go.Figure()
+      self.fig.update_layout(width=500, height=500)
+    else:
+      pass
+
+
+    min_x = min([circle.x-circle.r for circle in bubbles])
+    max_x = max([circle.x+circle.r for circle in bubbles])
+
+    max_y = max([circle.y+circle.r for circle in bubbles])
+    min_y = min([circle.y-circle.r for circle in bubbles])
+    height = 2*max(abs(max_y),abs(min_y))
+    width = max_x - min_x
+    
+
+    fig_width = self.fig.layout.width
+    self.fig.update_layout(height=fig_width*height/width)
+
+    min_x = -min_x
+
+    # padding
+    if padding:
+      self.padding = padding
+
+    # print(bubbles)
+    
+    points = [go.layout.Shape(x0=circle.x-(circle.r-self.padding)+min_x,
+                          y0=circle.y-(circle.r-self.padding), 
+                          x1=circle.x+(circle.r-self.padding)+min_x, 
+                          y1=circle.y+(circle.r-self.padding), 
+                          fillcolor=circle.ex['color'], 
+                          line_color=circle.ex['color'],
+                          **self.default_bubble_kwargs) for idx, circle in enumerate(bubbles) if circle.level==2]
+
+    self.fig.update_layout(shapes=points)
+    
+    self.fig.update_xaxes(range=[0, width])
+    self.fig.update_yaxes(range=[-height/2, height/2 ])
+
+    self.fig.update_xaxes(visible=False)
+    self.fig.update_yaxes(visible=False)
+    return self.fig
